34a49e6430bced3bfd45c60d845acbc2c406fbed
jki12 <sjak0227@naver.com>
2024-10-14 22:54:35 +0900

    feat: node factory 구현

---
+++ src/main/java/node/NodeFactory.java
@@ -0,0 +1,28 @@
+package node;
+
+import lombok.extern.slf4j.Slf4j;
+import node.functional.DebugNode;
+import node.functional.InjectNode;
+import node.functional.MarketIndexFetchNode;
+import node.functional.TcpServerNode;
+
+@Slf4j
+public class NodeFactory {
+
+    public static BaseNode createNode(Type type) {
+        BaseNode node;
+
+        if (type == Type.DEBUG) node = new DebugNode();
+        else if (type == Type.INJECT) node = new InjectNode();
+        else if (type == Type.TCP_SERVER) node = new TcpServerNode();
+        else if (type == Type.MARKET_INDEX_FETCH) node = new MarketIndexFetchNode();
+        else {
+            log.error("node type : {}", type);
+            throw new IllegalStateException("지원하지 않는 노드 입니다.");
+        }
+
+        log.info("{}가 생성 되었습니다", node.getClass().getSimpleName());
+
+        return node;
+    }
+}
