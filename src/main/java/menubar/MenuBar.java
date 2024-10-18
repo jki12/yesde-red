75129bb21004d58443fdbc07ed09fbbd3bdc8498
jki12 <sjak0227@naver.com>
2024-10-18 23:05:27 +0900

    chore: 불필요한 import 삭제, FileMenu 패키지 변경에 의한 import 추가

--- src/main/java/menubar/MenuBar.java
+++ src/main/java/menubar/MenuBar.java
@@ -1,15 +1,14 @@
 package menubar;
 
-import lombok.Getter;
+import menubar.file.FileMenu;
+import menubar.run.RunMenu;
 
 import javax.swing.*;
 
-@Getter
 public class MenuBar extends JMenuBar {
 
     public MenuBar() {
         add(new FileMenu());
         add(new RunMenu());
-
     }
 }
