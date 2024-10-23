d2d0f12ab6cb04987f5be2f92e9c3062cbcc2827
jki12 <sjak0227@naver.com>
2024-10-23 22:16:20 +0900

    feat: 가우스 소거법 로직 구현

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -4,14 +4,18 @@ import content.BoardPanel;
 import frame.YesdeRedFrame;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.swing.*;
+import java.awt.*;
 import java.net.http.HttpClient;
 import java.time.Duration;
 import java.util.regex.Pattern;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Utils {
+    private static final long MAX_POW = 2097152;
     private static final long TIMEOUT_DURATION = 5;
     private static final Pattern NUMERIC_PATTERN = Pattern.compile("^[0-9]*$");
 
@@ -52,5 +56,56 @@ public class Utils {
         }
 
         return x;
+    }
+
+    /**
+     * Point a, b 좌표를 기준으로 3차 함수의 계수를 구합니다.
+     */
+    public static double[] gaussianElimination(Point a, Point b) {
+        double[][] matrix = {
+                { Math.pow(a.x, 3), Math.pow(a.x, 2), a.x, 1, a.y },
+                { Math.pow(b.x, 3), Math.pow(b.x, 2), b.x, 1, b.y },
+                { 0, 3 * Math.pow(a.x, 2), 2 * a.x, a.x, 0 },
+                { 0, 3 * Math.pow(b.x, 2), 2 * b.x, b.x, 0 }
+        };
+
+        double temp;
+        for (int i = 0; i < matrix.length; ++i) {
+            double[] cur = matrix[i];
+
+            // leading 1을 만들어준다.
+            if (cur[i] != 1) {
+                for (int j = cur.length - 1; j >= 0; --j) cur[j] /= cur[i];
+            }
+
+            // 기약 사다리꼴을 만들기 위한 로직.
+            for (int j = i + 1; j < matrix.length; ++j) {
+                if (matrix[j][i] == 0) continue;
+
+                temp = matrix[j][i];
+                for (int k = 0; k < cur.length; ++k) {
+                    matrix[j][k] -= (cur[k] * temp);
+                }
+            }
+        }
+
+        // 기약 사다리꼴을 만들기 위한 로직, 이미 leading 1이 만들어져 있으므로 기약 사다리꼴을 만들기 위한 로직만 있으면 됨.
+        for (int i = matrix.length - 1; i >= 0; --i) {
+            double[] cur = matrix[i];
+
+            for (int j = i - 1; j >= 0; --j) {
+                if (matrix[j][i] == 0) continue;
+
+                temp = matrix[j][i];
+                for (int k = cur.length - 1; k >= 0; --k) {
+                    matrix[j][k] -= (cur[k] * temp);
+                }
+            }
+        }
+
+        double[] result = new double[matrix.length];
+        for (int i = 0; i < matrix.length; ++i) result[i] = matrix[i][matrix[0].length - 1];
+
+        return result;
     }
 }
