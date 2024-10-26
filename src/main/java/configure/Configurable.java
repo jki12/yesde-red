bdc459f562a4d4ae00e8fe2efbcec7ee20a658df
jki12 <sjak0227@naver.com>
2024-10-26 21:15:20 +0900

    feat: 설정 패널에 보여줄 변수 구분을 위한 어노테이션

---
+++ src/main/java/configure/Configurable.java
@@ -0,0 +1,12 @@
+package configure;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Configurable {
+    FieldType type() default FieldType.TEXT;
+}
---
+++ src/main/java/configure/FieldType.java
@@ -0,0 +1,5 @@
+package configure;
+
+public enum FieldType {
+    TEXT, TEXT_AREA, CHECK_BOX, RADIO_BUTTON;
+}
