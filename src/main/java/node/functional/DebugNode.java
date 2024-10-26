efbd8c104edd5d1c96cce24c5c70865fcf5306da
jki12 <sjak0227@naver.com>
2024-10-26 16:34:44 +0900

    fix: 불필요한 주석, 테스트 코드 삭제, console에 출력하는 코드에서 디버그 패널에 나오도록 수정

--- src/main/java/node/functional/DebugNode.java
+++ src/main/java/node/functional/DebugNode.java
@@ -1,13 +1,14 @@
 package node.functional;
 
-import node.*;
-import sidebar.DebugPanel;
+import content.DebugPanel;
+import frame.YesdeRedFrame;
+import node.InputOutputNode;
+import node.Inputable;
+import node.Message;
+import node.Type;
 
-import javax.swing.*;
-import java.awt.*;
-
-public class DebugNode extends InputOutputNode implements Input {
-    private DebugPanel instance = DebugPanel.getInstance();
+public class DebugNode extends InputOutputNode implements Inputable {
+    private final DebugPanel instance = (DebugPanel) YesdeRedFrame.getInstance().getContentPane().getComponent(1);
 
     public DebugNode() {
         super(Type.DEBUG);
@@ -15,18 +16,11 @@ public class DebugNode extends InputOutputNode implements Input {
 
     @Override
     protected void process() throws Exception {
-        getIn().push(new Message("test"));
         Message message = getIn().poll();
-
-//        var test = new JLabel(message.toString());
-//        var box = Box.createVerticalBox();
-//        box.add(test);
-//        test.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         instance.add(message.toString());
 
-
-        instance.updateUI();
-        System.out.println("message = " + message);
+        instance.revalidate();
+        instance.repaint();
     }
 }
