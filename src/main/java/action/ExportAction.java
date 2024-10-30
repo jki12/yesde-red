2e33561426086ec749c8dcc32623e08c53711946
jki12 <sjak0227@naver.com>
2024-10-30 21:26:29 +0900

    chore: todo 추가

--- src/main/java/action/ExportAction.java
+++ src/main/java/action/ExportAction.java
@@ -5,6 +5,7 @@ import util.TabInfo;
 import util.Utils;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 @Slf4j
@@ -17,6 +18,10 @@ public class ExportAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         TabInfo tabInfo = Utils.getSelectedTabInfo();
+
+        for (Component c : tabInfo.getBoard().getComponents()) { // TODO, file에 쓰는 로직 작성.
+            log.info(Utils.toJson(c));
+        }
 
         // TODO, json 형태로 추출하는 코드, 저장 위치 정하는 file chooser처럼 dialog 구현.
         log.info("{} 보드가 추출되었습니다. (index : {})", tabInfo.getBoard().getName(), tabInfo.getSelectedIndex());
