46b177d5f560c6f23c5ee8f64425bb4ab8c3a0d0
jki12 <sjak0227@naver.com>
2024-11-14 21:27:04 +0900

    fix: expose 어노테이션 추가, full uri 추가 필드 추가 ..

--- src/main/java/net/HttpRequestPacket.java
+++ src/main/java/net/HttpRequestPacket.java
@@ -1,29 +1,45 @@
 package net;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
 @Getter
-public class HttpRequestData {
+public class HttpRequestPacket {
+    @Expose
     @SerializedName("ip.version")
     private Integer ipVersion;
 
+    @Expose
     @SerializedName("ip.dst")
     private String dstAddress;
 
+    @Expose
     @SerializedName("tcp.dstport")
     private Integer dstPort;
 
+    @Expose
     @SerializedName("http.request.method")
     private String method;
 
+    @Expose
     @SerializedName("http.request.uri")
     private String uri;
 
+    @Expose
     @SerializedName("http.request.version")
     private String httpVersion;
 
+    @Expose
+    @SerializedName("http.request.full_uri")
+    private String fullUri;
+
+//    @SerializedName("http.file_data") TODO, string, binary test.
+//    private byte[] payload;
+
+    @Setter
     private Map<String, String> headers;
 }
