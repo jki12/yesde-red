8c9456a0544063ceaf0f762459d5a50c716c6e92
jki12 <sjak0227@naver.com>
2024-11-21 15:04:57 +0900

    feat: connect 함수 추가

--- src/main/java/node/Outputable.java
+++ src/main/java/node/Outputable.java
@@ -4,4 +4,8 @@ package node;
  * marker 인터페이스 입니다.
  */
 public interface Outputable {
+
+    default void connect(Inputable input) {
+        ((InputOutputNode) this).tryConnect((InputOutputNode) input);
+    }
 }
